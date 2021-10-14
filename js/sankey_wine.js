4c2ebbfb9a408093489103a830ccdd4b4e33d70c
Adeline Chew <ache0072@student.monash.edu>
2021-10-14 15:28:53 +0800

    Updates

--- js/sankey_wine.js
+++ js/sankey_wine.js
@@ -4,71 +4,74 @@ var option;
 
 // Draw the chart
 myChart.showLoading();
-$.get("/json/sankey_data.json", function (data) {
-    myChart.hideLoading();
-    myChart.setOption(
-        (option = {
-            tooltip: {
-                trigger: "item",
-                triggerOn: "mousemove",
-            },
-            series: [
-                {
-                    type: "sankey",
-                    data: data.nodes,
-                    links: data.links,
-                    emphasis: {
-                        focus: "adjacency",
-                    },
-                    levels: [
-                        {
-                            depth: 0,
-                            itemStyle: {
-                                color: "#D5BFBF",
-                            },
-                            lineStyle: {
-                                color: "source",
-                                opacity: 0.6,
-                            },
-                        },
-                        {
-                            depth: 1,
-                            itemStyle: {
-                                color: "#8CA1A5",
-                            },
-                            lineStyle: {
-                                color: "source",
-                                opacity: 0.6,
-                            },
-                        },
-                        {
-                            depth: 2,
-                            itemStyle: {
-                                color: "#6D8299",
-                            },
-                            lineStyle: {
-                                color: "source",
-                                opacity: 0.6,
-                            },
-                        },
-                        {
-                            depth: 3,
-                            itemStyle: {
-                                color: "#316B83",
-                            },
-                            lineStyle: {
-                                color: "source",
-                                opacity: 0.6,
-                            },
-                        },
-                    ],
-                    lineStyle: {
-                        curveness: 0.5,
-                    },
+$.get(
+    "https://raw.githubusercontent.com/Adeline-Chew/Wine/main/json/sankey_data.json",
+    function (data) {
+        myChart.hideLoading();
+        myChart.setOption(
+            (option = {
+                tooltip: {
+                    trigger: "item",
+                    triggerOn: "mousemove",
                 },
-            ],
-        })
-    );
-});
+                series: [
+                    {
+                        type: "sankey",
+                        data: data.nodes,
+                        links: data.links,
+                        emphasis: {
+                            focus: "adjacency",
+                        },
+                        levels: [
+                            {
+                                depth: 0,
+                                itemStyle: {
+                                    color: "#D5BFBF",
+                                },
+                                lineStyle: {
+                                    color: "source",
+                                    opacity: 0.6,
+                                },
+                            },
+                            {
+                                depth: 1,
+                                itemStyle: {
+                                    color: "#8CA1A5",
+                                },
+                                lineStyle: {
+                                    color: "source",
+                                    opacity: 0.6,
+                                },
+                            },
+                            {
+                                depth: 2,
+                                itemStyle: {
+                                    color: "#6D8299",
+                                },
+                                lineStyle: {
+                                    color: "source",
+                                    opacity: 0.6,
+                                },
+                            },
+                            {
+                                depth: 3,
+                                itemStyle: {
+                                    color: "#316B83",
+                                },
+                                lineStyle: {
+                                    color: "source",
+                                    opacity: 0.6,
+                                },
+                            },
+                        ],
+                        lineStyle: {
+                            curveness: 0.5,
+                        },
+                    },
+                ],
+            })
+        );
+    }
+);
 
 option && myChart.setOption(option);
