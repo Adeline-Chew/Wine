f48ec2ec3df7d309c165fe81d55910280cb85180
Adeline Chew <ache0072@student.monash.edu>
2021-10-17 01:58:24 +0800

    Updates

--- js/vega.js
+++ js/vega.js
@@ -6,8 +6,11 @@ var spec4 =
     "https://raw.githubusercontent.com/Adeline-Chew/Wine/main/json/bar_popular_wine.json";
 var spec5 =
     "https://raw.githubusercontent.com/Adeline-Chew/Wine/main/json/box_price_wine.json";
+var spec6 =
+    "https://raw.githubusercontent.com/Adeline-Chew/Wine/main/json/pie_ingredients.json";
 // Embed the visualization in the container with id `vis`
 vegaEmbed("#viz1", spec1);
 vegaEmbed("#viz2", spec2);
 vegaEmbed("#viz4", spec4);
 vegaEmbed("#viz5", spec5);
+vegaEmbed("#viz6", spec6);
